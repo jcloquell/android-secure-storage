02c44bbeea6456f027b765cdf89ad5abe4fe4bc3
Jorge Cloquell Ribera <jorge@door2door.io>
2018-06-12 17:55:35 +0200

    Add first instrumentation test

--- app/src/androidTest/java/com/jcloquell/andriodsecurestoragetest/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/jcloquell/andriodsecurestoragetest/ExampleInstrumentedTest.kt
@@ -1,18 +1,33 @@
 package com.jcloquell.andriodsecurestoragetest
 
-import android.support.test.InstrumentationRegistry
+import android.support.test.espresso.Espresso.onView
+import android.support.test.espresso.action.ViewActions.*
+import android.support.test.espresso.matcher.ViewMatchers.withId
+import android.support.test.rule.ActivityTestRule
 import android.support.test.runner.AndroidJUnit4
-import org.junit.Assert.assertEquals
+import com.jcloquell.androidsecurestorage.SecureStorage
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
 
+  @Rule
+  var activityRule = ActivityTestRule(MainActivity::class.java)
+
   @Test
-  fun useAppContext() {
-    // Context of the app under test.
-    val appContext = InstrumentationRegistry.getTargetContext()
-    assertEquals("com.jcloquell.andriodsecurestoragetest", appContext.packageName)
+  fun should() {
+    val secureStorage = SecureStorage(activityRule.activity.applicationContext)
+    val text = "text"
+
+    onView(withId(R.id.stringEditText))
+        .perform(typeText(text))
+        .perform(closeSoftKeyboard())
+    onView(withId(R.id.encryptStringButton))
+        .perform(click())
+
+    assertThat(text).isEqualTo(secureStorage.getObject("stringKey", String::class.java))
   }
 }
